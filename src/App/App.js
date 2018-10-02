8ed7556707fe8f8ebd6699f34d6b3a6e332e24e4
Renato Böhler <renato.bohler@gmail.com>
2018-10-01 22:14:12 -0300

    adiciona material-ui ao projeto

---
+++ src/App/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+export default () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+);

59603800c7d203d41ed8cc821bf68710d1347989
Renato Böhler <renato.bohler@gmail.com>
2018-10-05 22:35:27 -0300

    adiciona prop action ao componente Empty

--- src/components/Empty/Empty.jsx
+++ src/components/Empty/Empty.jsx
@@ -9,7 +9,7 @@ import { Cancel } from '@material-ui/icons';
 const styles = theme => ({
   container: { textAlign: 'center', marginTop: 64 },
   icon: { width: 64, height: 64, color: theme.palette.common.gray, margin: 'auto' },
-  text: { color: theme.palette.common.gray },
+  text: { color: theme.palette.common.gray, marginBottom: 16 },
 });
 
 const Empty = props => (
@@ -18,16 +18,19 @@ const Empty = props => (
     <Typography variant="title" className={props.classes.text}>
       {props.children}
     </Typography>
+    {props.action}
   </div>
 );
 
 Empty.propTypes = {
   children: PropTypes.string,
+  action: PropTypes.node,
   classes: PropTypes.object.isRequired,
 };
 
 Empty.defaultProps = {
   children: '',
+  action: '',
 };
 
 export default withStyles(styles)(Empty);

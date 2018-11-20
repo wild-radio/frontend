f294fbc10396888a3cb67dde7dac25ff6d791729
killertux <bruno.canguru@hotmail.com>
2018-11-20 19:31:24 -0200

    Update build

--- src/utils/axiosInterceptor.js
+++ src/utils/axiosInterceptor.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 import * as appThunks from '../components/App/store/thunks';
 
 export default () => dispatch => {
+    axios.interceptor.request.use(config => {
+        console.log('config', config);
+    }, error => {
+        debugger;
+        console.log('error', error);
+    });
+
   axios.interceptors.response.use(
     response => response,
     error => {
-        console.log(error);
+        debugger;
       if (error && error.response && error.response.data) {
         dispatch(appThunks.showSnackbar(error.response.data, 'error'));
       } else {
